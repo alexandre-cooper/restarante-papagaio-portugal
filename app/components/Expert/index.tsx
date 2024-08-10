4d19359595cfebc6e827c754bcef226c8e5296b1
Alexandre Brasil <alexandre.brazil.official@gmail.com>
2024-08-10 02:20:14 +0100

    feat: localization

--- app/components/Expert/index.tsx
+++ app/components/Expert/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Slider from "react-slick";
 import React, { Component } from "react";
 import Image from "next/image";
@@ -7,90 +7,97 @@ import { Fade } from "react-awesome-reveal";
 // CAROUSEL DATA
 
 interface DataType {
-    profession: string;
-    name: string;
-    imgSrc: string;
+  profession: string;
+  name: string;
+  imgSrc: string;
 }
 export const metadata = {
-    title: 'Localização',
-  }
-
+  title: "Localização",
+};
 
 export default class MultipleItems extends Component {
-
-    render() {
-        const settings = {
-            dots: false,
-            infinite: true,
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      slidesToShow: 3,
+      // centerMode: true,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: false,
+      speed: 4000,
+      autoplaySpeed: 2000,
+      cssEase: "linear",
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
             slidesToShow: 3,
-            // centerMode: true,
             slidesToScroll: 1,
-            arrows: false,
-            autoplay: false,
-            speed: 4000,
-            autoplaySpeed: 2000,
-            cssEase: "linear",
-            responsive: [
-                {
-                    breakpoint: 1200,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: false
-                    }
-                },
-                {
-                    breakpoint: 800,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: false
-                    }
-                },
-                {
-                    breakpoint: 450,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: false
-                    }
-                }
-            ]
-        };
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 450,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          },
+        },
+      ],
+    };
 
-
-        return (
-            <div className="py-10 sm:py-20 bg-darkpink">
-
-                <div className='mx-auto max-w-2xl lg:max-w-7xl sm:py-4 lg:px-8'>
-                    <div className="text-center">
-                        <Fade direction={'up'} delay={400} cascade damping={1e-1} triggerOnce={true}>
-                            <h2 className='text-pink text-lg font-normal mb-3 tracking-widest uppercase ls-51'>Onde nos estamos?</h2>
-                        </Fade>
-                        <Fade direction={'up'} delay={800} cascade damping={1e-1} triggerOnce={true}>
-                            <h3 className="text-3xl lg:text-5xl font-semibold text-black">
-                                Faça-nos uma visita
-                            </h3>
-                        </Fade>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-6 md:gap-8 max-w-7xl mx-auto mt-28 mb-8 justify-center items-center w-full">
-                <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6009.322716286264!2d-8.615875852127559!3d41.1419180150414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd24651d6e848255%3A0x35e10b16b3dec939!2sRestaurante%20churrasqueira%20papagaio!5e0!3m2!1spt-PT!2spt!4v1722556509241!5m2!1spt-PT!2spt"
-                width="1280"
-                height="900"
-                style={{ border: 0 }}
-                allowFullScreen
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-                >
-                </iframe>
-                </div>
-            </div>
-
-        );
-    }
+    return (
+      <div className="py-10 sm:py-20 bg-darkpink">
+        <div className="mx-auto max-w-2xl lg:max-w-7xl sm:py-4 lg:px-8">
+          <div className="text-center">
+            <Fade
+              direction={"up"}
+              delay={400}
+              cascade
+              damping={1e-1}
+              triggerOnce={true}
+            >
+              <h2 className="text-pink text-lg font-normal mb-3 tracking-widest uppercase ls-51">
+                Onde nos estamos?
+              </h2>
+            </Fade>
+            <Fade
+              direction={"up"}
+              delay={800}
+              cascade
+              damping={1e-1}
+              triggerOnce={true}
+            >
+              <h3 className="text-3xl lg:text-5xl font-semibold text-black">
+                Faça-nos uma visita
+              </h3>
+            </Fade>
+          </div>
+        </div>
+        <div className="flex flex-col gap-6 md:gap-8 max-w-7xl mx-auto mt-28 mb-8 justify-center items-center w-full">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6009.322716286264!2d-8.615875852127559!3d41.1419180150414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd24651d6e848255%3A0x35e10b16b3dec939!2sRestaurante%20churrasqueira%20papagaio!5e0!3m2!1spt-PT!2spt!4v1722556509241!5m2!1spt-PT!2spt"
+            className="w-full h-96 md:h-[500px]"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+    );
+  }
 }
